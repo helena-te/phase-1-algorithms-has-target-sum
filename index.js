59158bb09afd5a3ef144796b03afca65f0466366
Helena Tenev <htenev14@gmail.com>
2023-01-17 18:20:04 -0500

    commit tree a7a5145f366cc6977c3c4f419f09bbb297afbf9f

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+const arrayLength = array.length
+
+for (i=0; i < arrayLength; i++) {
+  for (j=0; j < arrayLength; j++) {
+    if (i!=j) {
+      const sum = array[i] + array[j];
+       console.log(i,j,sum)
+       if (sum == target) {
+        return true
+      }
+      // else {
+      //   console.log("nope") 
+      // }
+    }
+  
+ // console.log(i,arrayLength,j)
+ 
+//   }
+//   if (yup == true) {
+//     return true
+//    } 
+// }
+
+  }
+  
+}
+return false
 }
 
 /* 
@@ -8,6 +35,13 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  1. sum first number and all other array numbers, respectively
+  2. if any of those sums equal target, return true. if not, go to step 3
+  3. sum second number and all other array numbers, respectively
+  4. repeat step 2
+  5. if you've summed last number with all other array numbers with none equal to target,
+    return false
+
 */
 
 /*
@@ -20,15 +54,15 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+  // console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+  // console.log("Expecting: true");
+  // console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+  // console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+  // console.log("Expecting: false");
+  // console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
